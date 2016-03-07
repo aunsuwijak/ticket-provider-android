0844a0ca4826a80105b4544a296de9e32f0885db
TAWEESOFT <taweesoft@gmail.com>
2016-03-07 16:09:19 +0700

    [#113546497] Remove files from utils

--- app/src/main/java/com/example/taweesoft/ticketprovider/ui/views/MainActivity.java
+++ app/src/main/java/com/example/taweesoft/ticketprovider/ui/views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.taweesoft.ticketprovider;
+package com.example.taweesoft.ticketprovider.ui.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.taweesoft.ticketprovider.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/taweesoft/ticketprovider/utils/Storage.java
+++
@@ -1,8 +0,0 @@
-package com.example.taweesoft.ticketprovider.utils;
-
-/**
- * Created by TAWEESOFT on 3/5/16 AD.
- */
-public class Storage {
-    public static String grant_type = "password";
-}
