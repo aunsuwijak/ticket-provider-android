cba32e88553fa374cc92169ccf6e57ff1ba39010
TAWEESOFT <taweesoft@gmail.com>
2016-03-12 17:16:26 +0700

    [#115107217] Added url to Constants.java

--- app/src/staging/java/com/example/taweesoft/ticketprovider/Constants.java
+++ app/src/staging/java/com/example/taweesoft/ticketprovider/Constants.java
@@ -4,5 +4,5 @@ package com.example.taweesoft.ticketprovider;
  * Created by TAWEESOFT on 3/12/16 AD.
  */
 public class Constants {
-    public static String BASE = "Staging";
+    public static String BASE_URL = "https://ticket-provider-staging.herokuapp.com/";
 }
