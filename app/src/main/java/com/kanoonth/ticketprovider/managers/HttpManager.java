9c1b23443f8c801e853579251c8e78d1d8e3c48f
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-03-22 15:30:53 +0700

    [#115833577][#113546527] Setup Realm

---
+++ app/src/main/java/com/kanoonth/ticketprovider/managers/HttpManager.java
@@ -0,0 +1,49 @@
+package com.kanoonth.ticketprovider.managers;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.kanoonth.ticketprovider.Constants;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import io.realm.RealmObject;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
+/**
+ * Created by TAWEESOFT on 3/3/16 AD.
+ */
+public class HttpManager {
+
+    private static HttpManager instance = null;
+
+    public static HttpManager getInstance() {
+        if (instance == null)
+            instance = new HttpManager();
+        return instance;
+    }
+
+    public static APIService getApiService() {
+        Gson gson = new GsonBuilder()
+                .setExclusionStrategies(new ExclusionStrategy() {
+                    @Override
+                    public boolean shouldSkipField(FieldAttributes f) {
+                        return f.getDeclaringClass().equals(RealmObject.class);
+                    }
+
+                    @Override
+                    public boolean shouldSkipClass(Class<?> clazz) {
+                        return false;
+                    }
+                })
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        APIService service = retrofit.create(APIService.class);
+        return service;
+    }
+}
